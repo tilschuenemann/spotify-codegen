ed853d702b9e773df29959469636e2ec7fee6caf
schuenemann <til.schuenemann@mailbox.org>
2022-07-02 17:10:55 +0200

    added another mode for getting all album covers in library

--- bbsac/bbsac/bbsac.py
+++ bbsac/bbsac/bbsac.py
@@ -2,6 +2,7 @@ from colorthief import ColorThief
 from PIL import Image
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 import tqdm
 
 import argparse
@@ -14,7 +15,7 @@ def rgb_to_hex(rgb):
     return "%02x%02x%02x" % rgb
 
 
-def generate_album_codes(output_folder: str, album_uris: str):
+def generate_album_codes(output_folder: str, album_uris: list[str],sp: spotipy.Spotify):
     """Generates images for each supplied album_uri that merges
     the album cover and its Spotify code.
 
@@ -25,21 +26,6 @@ def generate_album_codes(output_folder: str, album_uris: str):
     album_uris: str
         list of album uris
     """
-    if output_folder is None:
-        output_folder = os.path.dirname(os.path.realpath(__file__))
-    else:
-        if os.path.isdir(output_folder) is False:
-            print("supplied path is not a folder")
-            exit()
-
-    if not (album_uris and album_uris.strip()):
-        exit("uris are blank")
-    else:
-        album_uris = album_uris.split(",")
-
-    # SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET need to be exported first
-    sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-
     for album_uri in tqdm.tqdm(album_uris, desc="generating album codes "):
 
         # get album cover from spotify API
@@ -84,6 +70,7 @@ def generate_album_codes(output_folder: str, album_uris: str):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    
     parser.add_argument(
         "-u",
         dest="uris",
@@ -95,6 +82,44 @@ if __name__ == "__main__":
     parser.add_argument(
         "-o", dest="out", type=str, required=False, help="path to output folder"
     )
+
+    parser.add_argument("-a",dest="all_albums",action="store_true",required=False,help="flag for fetching all albums in your music library")
     args = parser.parse_args()
 
-    generate_album_codes(args.out, args.uris[0])
+    # add current folder as fallback output folder
+    if args.out is None:
+        output_folder = os.path.dirname(os.path.realpath(__file__))
+    else:
+        output_folder = args.out
+    
+    if os.path.isdir(output_folder) is False:
+        exit("supplied path is not a folder")
+
+    if args.all_albums is False:
+        if not (album_uris and album_uris.strip()):
+            exit("uris are blank")
+        else:
+            uri_list = album_uris.split(",")
+
+        # SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET need to be exported first
+        sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
+    else:
+        scope = "user-library-read"
+        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+            scope=scope, redirect_uri="http://127.0.0.1:9090"))
+
+        # 50 item limit on getting saved albums, this adds the uris in chunks to uri_list
+        items = True
+        offset = 0
+        uri_list = []
+        while items:
+            results = sp.current_user_saved_albums(limit=50, offset=offset)
+            for album in results["items"]:
+                uri_list.append(album["album"]["uri"])
+            offset+=50
+            if len(results["items"])==0:
+                items = False
+
+
+    generate_album_codes(output_folder, uri_list, sp)
