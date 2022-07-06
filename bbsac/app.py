1d428c4ebad9ad5eb7641eb5cfe1eb34675225ae
schuenemann <til.schuenemann@mailbox.org>
2022-07-06 12:55:05 +0200

    added prototype for regular text search

--- bbsac/app.py
+++ bbsac/app.py
@@ -1,6 +1,5 @@
-from dash import Dash, dcc, html, Input, Output
-from dash import Dash, dcc, html, Input, Output
-from bbsac.bbsac import get_album_code
+from dash import Dash, dcc, html, Input, Output, State
+from bbsac import get_album_code
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
@@ -10,7 +9,8 @@ from urllib.request import urlopen
 import base64
 import re
 
-app = Dash(__name__,external_stylesheets=["https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css"])
+app = Dash(__name__, external_stylesheets=[
+           "https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css"])
 
 server = app.server
 
@@ -44,27 +44,62 @@ app.index_string = '''
 '''
 
 app.layout = html.Div([
-    html.H1("Bring Back Spotify Album Codes",className="text-4xl"),
-    dcc.Input(id="uri", type="text",debounce=True,
-              placeholder="Enter Spotify Album URI here!"),
-    html.Div(id="picture")],className="grid justify-items-center p-4 w-full h-screen bg-violet-800")
+    html.Div(
+        ["Search By", dcc.Dropdown(id="search_by", options=[
+            "metadata", "uri"], value="metadata"),
+         ], className="w-64"),
+    html.Div([
+        html.Div([html.H1("Search by Metadata"),
+                  dcc.Dropdown(id="search_type", options=[
+                      "album", "artist", "track"]),
+                  dcc.Input(id="search_general", type="text", debounce=True,
+                            placeholder="General Search:"),
+                  html.Button('Search!', id='search-trigger',
+                              n_clicks=0, className="bg-emerald-400 w-64"),
+                  ], className="grid align-center"),
+        html.Div([html.H1("Search by URI"),
+                  dcc.Input(id="search_uri", type="text", debounce=True,
+                            placeholder="Enter Spotify Album URI here!")]),
+        html.Div(id="picture"),
+
+        dcc.Store(id="search_uriresult")], className="grid")])
 
 
 sp = spotipy.Spotify(
-            client_credentials_manager=SpotifyClientCredentials())
+    client_credentials_manager=SpotifyClientCredentials())
 
 
-@app.callback(Output("picture", "children"), Input("uri", "value"))
-def load_image(uri):
+@ app.callback(Output("picture", "children"),
+               State("search_by", "value"),
+               State("search_type", "value"),
+               State("search_uri", "value"),
+               State("search_general","value"),
+               Input('search-trigger', 'n_clicks'),
+               
+               prevent_initial_call=True)
+def search(search_by, search_type, search_uri,search_general, n_clicks,):
 
-    if uri is None:
-        return html.Div()
+    if search_by == "uri":
+        if search_uri is not None:
+            uri = search_uri
+        else:
+            return html.Div()
 
-    if re.match(r"^spotify:album:[A-Za-z0-9]{22}$", uri): 
-        img = get_album_code(uri, sp)
-        return html.Img(src=img)
-    else:
-        return html.Span("Please supply a valid Spotify album URI!")
+    elif search_by == "metadata":
+        if search_type is None or search_general is None:
+            return html.Div()
+
+        results = sp.search(q=search_general, limit=1, type=search_type)
+        if search_type == "album":
+            uri = results["albums"]["items"][0]["uri"]
+        elif search_type == "track":
+            uri = results["tracks"]["items"][0]["uri"]
+        elif search_type == "artist":
+            uri = results["artists"]["items"][0]["uri"]
+
+    img = get_album_code(uri, sp)
+    return html.Img(src=img)
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
